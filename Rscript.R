d72d0dc8f56bff6f0051779d4be097f8f48a732e
Sthiriet-rupert <86595215+Sthiriet-rupert@users.noreply.github.com>
2022-11-03 17:50:08 +0100

    Update Rscript.R

--- Rscript.R
+++ Rscript.R
@@ -63,19 +63,27 @@ ggsave("Figures/Boxplot_Anova_CloneMICs_ColorPoints.tiff",width=7,height = 7,dev
 
 
 ## Heatmaps for mutated genes in the final populations
-# (same for the heatmaps with all SNPs)
-
+# Biofilm
 
 data<-read.table("R_data/Mut_Genes_Cycle10_Biofilm.txt",header = T,sep = "\t")
 
 library(reshape2)
 library(ggplot2)
+library(dplyr)
 
 names<-data$Mutation
 namesy<-rev(names)
 
 
-dat<-melt(data=data,id.vars = c("Mutation"),measure.vars = c("X1","X2","X3","X4","X5","X6","X7","X8","X9"),variable.name = "Sample",value.name = "Freq")
+dat<-melt(data=data,id.vars = c("Mutation"),measure.vars = c("B1","B2","B3","B4","B5","B6","B7","B8","B9"),variable.name = "Sample",value.name = "Freq")
+
+labs <- sapply(strsplit(as.character(namesy), " "), 
+               FUN = function(x) {
+                   x1 <- x[1]; x2 <- x[2]
+                   parse(text = paste("plain('", x1, "') ~ ", "italic('", x2, "')", sep = ""))
+               })
+
+s = subset(dat, Freq > 5)
 
 hp<-ggplot(data = dat, aes(x=Sample, y=Mutation, fill=Freq)) + 
     geom_tile(color = "black")+
@@ -99,10 +107,158 @@ hp + theme(
     legend.position="bottom",
     legend.direction = "horizontal")+
     guides(fill = guide_colorbar(barwidth = 12, barheight = 1,
-                                 title.vjust = 0.9,title.hjust = 0))   + scale_x_discrete(position = "top")+  scale_y_discrete(limits=namesy)
+                                 title.vjust = 0.9,title.hjust = 0))   + scale_x_discrete(position = "top")+  scale_y_discrete(labels=labs,breaks=namesy,limits=namesy)+geom_text(data=s,aes(Sample,Mutation,label=Freq))
 
 ggsave("Figures/Heatmap_Genes_Biof.tiff",width=7,height = 10,device = "tiff",dpi = 700)
 
+
+
+# Planktonic
+
+data<-read.table("R_data/Mut_Genes_Cycle10_Plk.txt",header = T,sep = "\t")
+
+library(reshape2)
+library(ggplot2)
+library(dplyr)
+
+names<-data$Mutation
+namesy<-rev(names)
+
+
+dat<-melt(data=data,id.vars = c("Mutation"),measure.vars = c("P1","P2","P3","P4","P5","P6","P7","P8","P9"),variable.name = "Sample",value.name = "Freq")
+
+labs <- sapply(strsplit(as.character(namesy), " "), 
+               FUN = function(x) {
+                   x1 <- x[1]; x2 <- x[2]
+                   parse(text = paste("plain('", x1, "') ~ ", "italic('", x2, "')", sep = ""))
+               })
+
+s = subset(dat, Freq > 5)
+
+hp<-ggplot(data = dat, aes(x=Sample, y=Mutation, fill=Freq)) + 
+    geom_tile(color = "black")+
+    scale_fill_gradientn(values=c(1, .5, 0), colours=c("red", "#f08080", "white"), limit = c(0,100),
+                         name="Frequency") + theme_minimal()+ 
+    theme(axis.text.x = element_text(vjust = 1, 
+                                     size = 12, hjust = 1))+
+    coord_fixed()
+
+
+hp + theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    axis.text.y = element_text(size=12),
+    axis.text.x = element_text(hjust = 0.5),
+    panel.grid.major = element_blank(),
+    panel.border = element_blank(),
+    panel.background = element_blank(),
+    axis.ticks = element_blank(),
+    legend.justification = c(1, 0),
+    legend.position="bottom",
+    legend.direction = "horizontal")+
+    guides(fill = guide_colorbar(barwidth = 12, barheight = 1,
+                                 title.vjust = 0.9,title.hjust = 0))   + scale_x_discrete(position = "top")+  scale_y_discrete(labels=labs,breaks=namesy,limits=namesy)+geom_text(data=s,aes(Sample,Mutation,label=Freq))
+
+ggsave("Figures/Heatmap_Genes_Plk.tiff",width=7,height = 10,device = "tiff",dpi = 700)
+
+
+## Same heatmap but for all identified mutations
+# Biofilm
+
+
+data<-read.table("R_data/All_Mut_Biofilm",header = T,sep = "\t")
+library(reshape2)
+library(ggplot2)
+library(dplyr)
+
+names<-data$Mutation
+namesy<-rev(names)
+
+
+dat<-melt(data=data,id.vars = c("Mutation"),measure.vars = c("B1","B2","B3","B4","B5","B6","B7","B8","B9"),variable.name = "Sample",value.name = "Freq")
+
+labs <- sapply(strsplit(as.character(namesy), " "), 
+               FUN = function(x) {
+                   x1 <- x[1]; x2 <- x[2]; x3 <- x[3]
+                   parse(text = paste("italic('", x1, "') ~ ", "plain('", x2, "') ~ ", "plain('", x3, "')", sep = ""))
+               })
+
+s = subset(dat, Freq > 5)
+
+hp<-ggplot(data = dat, aes(x=Sample, y=Mutation, fill=Freq)) + 
+    geom_tile(color = "black")+
+    scale_fill_gradientn(values=c(1, .5, 0), colours=c("red", "#f08080", "white"), limit = c(0,100),
+                         name="Frequency") + theme_minimal()+ 
+    theme(axis.text.x = element_text(vjust = 1, 
+                                     size = 12, hjust = 1))+
+    coord_fixed()
+
+
+hp + theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    axis.text.y = element_text(size=12),
+    axis.text.x = element_text(hjust = 0.5),
+    panel.grid.major = element_blank(),
+    panel.border = element_blank(),
+    panel.background = element_blank(),
+    axis.ticks = element_blank(),
+    legend.justification = c(1, 0),
+    legend.position="bottom",
+    legend.direction = "horizontal")+
+    guides(fill = guide_colorbar(barwidth = 12, barheight = 1,
+                                 title.vjust = 0.9,title.hjust = 0))   + scale_x_discrete(position = "top")+  scale_y_discrete(labels=labs,breaks=namesy,limits=namesy)+geom_text(data=s,aes(Sample,Mutation,label=Freq))
+                                 
+ggsave("Figures/Heatmap_AllMut_Biof.tiff",width=12,height = 12,device = "tiff",dpi = 700)
+
+
+# Planktonic
+
+
+data<-read.table("R_data/All_Mut_Planktonic",header = T,sep = "\t")
+library(reshape2)
+library(ggplot2)
+library(dplyr)
+
+names<-data$Mutation
+namesy<-rev(names)
+
+
+dat<-melt(data=data,id.vars = c("Mutation"),measure.vars = c("P1","P2","P3","P4","P5","P6","P7","P8","P9"),variable.name = "Sample",value.name = "Freq")
+
+labs <- sapply(strsplit(as.character(namesy), " "), 
+               FUN = function(x) {
+                   x1 <- x[1]; x2 <- x[2]; x3 <- x[3]
+                   parse(text = paste("italic('", x1, "') ~ ", "plain('", x2, "') ~ ", "plain('", x3, "')", sep = ""))
+               })
+
+s = subset(dat, Freq > 5)
+
+hp<-ggplot(data = dat, aes(x=Sample, y=Mutation, fill=Freq)) + 
+    geom_tile(color = "black")+
+    scale_fill_gradientn(values=c(1, .5, 0), colours=c("red", "#f08080", "white"), limit = c(0,100),
+                         name="Frequency") + theme_minimal()+ 
+    theme(axis.text.x = element_text(vjust = 1, 
+                                     size = 12, hjust = 1))+
+    coord_fixed()
+
+
+hp + theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    axis.text.y = element_text(size=12),
+    axis.text.x = element_text(hjust = 0.5),
+    panel.grid.major = element_blank(),
+    panel.border = element_blank(),
+    panel.background = element_blank(),
+    axis.ticks = element_blank(),
+    legend.justification = c(1, 0),
+    legend.position="bottom",
+    legend.direction = "horizontal")+
+    guides(fill = guide_colorbar(barwidth = 12, barheight = 1,
+                                 title.vjust = 0.9,title.hjust = 0))   + scale_x_discrete(position = "top")+  scale_y_discrete(labels=labs,breaks=namesy,limits=namesy)+geom_text(data=s,aes(Sample,Mutation,label=Freq))
+                                 
+ggsave("Figures/Heatmap_AllMut_Plk.tiff",width=12,height = 12,device = "tiff",dpi = 700)
 
 
 ## Boxplot entropy
